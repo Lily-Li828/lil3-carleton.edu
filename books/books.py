8039d6f4ab9c6219f106b55016e9251afb3d1d75
Lily <lilyli828@DESKTOP-KFGTU6J.localdomain>
2020-09-23 10:46:13 -0700

    Added operation which prints operation

--- books/books.py
+++ books/books.py
@@ -4,79 +4,91 @@ import csv
 
 #function which print out titles of books according to strings it contains.
 
-def search_title(string):
-  with open('books.csv', newline='') as k:
-    reader = csv.reader(k)
-    for row in reader:
-      title = row[0]
-      if(string in title.lower()):
-        print(title)
+
+def search_title(string,reader):
+  for row in reader:
+    title = row[0]
+    if(string in title.lower()):
+      print(title)
 
 
 #function which print out titles of books according to year of publication.
-def search_year(startY, endY):
-  with open('books.csv', newline='') as k:
-    reader = csv.reader(k)
+def search_year(startYear, endYear,reader):
+  if(startYear>endYear):
+    print("Please enter search years in correct format: \n [starting year] [ending year]")
+  else:
     for row in reader:
       year = int(row[1])
-      if(year>= startY and year <= endY):
+      if(year>= startYear and year <= endYear):
         print(row[0])
 
 
 #function which print out authors' names and books according to string in their name
 
-def search_author(string):
- with open('books.csv', newline='') as k:
-    reader = csv.reader(k)
-    namelist = []
-    for row in reader:
-      author = row[2]
-      if(string in author.lower() and author not in namelist):
-        print('\033[1m'+author+'\033[0m')
-        namelist.append(author)
-        with open('books.csv', newline='') as j:
-          readerr = csv.reader(j)
-          for row in readerr:
-            if(row[2]==author):
-              print('\033[94m'+row[0]+'\033[0m')
-          print('')
+def search_author(string,reader):
+  namelist = []
+  for row in reader:
+    author = row[2]
+    if(string in author.lower() and author not in namelist):
+      print('\033[1m'+author+'\033[0m')
+      namelist.append(author)
+      with open('books.csv', newline='') as j:
+        reader2 = csv.reader(j)
+        for row in reader2:
+          if(row[2]==author):
+            print('\033[94m'+row[0]+'\033[0m')
+        print('')
+
+# prints out syntax dictionary
+def help_menu():
+  helpMenu= open(r"usage.txt","r")
+  for row in helpMenu:
+    print(row)
+  helpMenu.close()
      
       
 #implementation of different functions according to users' input
-  
-try:
-  if (sys.argv[1] == 'st' or sys.argv[1] == 'search-title'  ):
-    try:
-      string = sys.argv[2]
-      print('\033[4m'+"Below are book titles containing "+string+'\033[0m')
-      search_title(string)
-    except:
-      print("Please enter the string you would like to search")
 
-  elif(sys.argv[1] == 'sy' or sys.argv[1] == 'search-year'):
-    try:
-      startY = int(sys.argv[2]) 
-      endY = int(sys.argv[3]) 
-      print('\033[4m'+"Below are books publish between ",startY ,"and " ,endY,'\033[0m')
-      search_year(startY, endY)
-    except:
-      print("Please enter the range of publishing years you would like to search, in formate [start year] [end year] ")
+def main():
+  reader = csv.reader(open('books.csv'))
+  try:
+    if(sys.argv[1] == 'h' or sys.argv[1] == 'help'  ):
+      help_menu()      
+    elif (sys.argv[1] == 'st' or sys.argv[1] == 'search-title'  ):
+      try:
+        string = sys.argv[2]
+        print('\033[4m'+"Below are book titles containing "+string+'\033[0m')
+        search_title(string,reader)
+      except:
+        print("Please enter the string you would like to search")
 
-  elif(sys.argv[1] == 'sa' or sys.argv[1] == 'search-author'):
-    try:
-      string = sys.argv[2]
-      print("Below are authors with names containing "+string)
-      print("publications of the author are in purple ")
-      print('')
-      search_author(string)
-    except:
-      print("Please enter the string which you would like the authors' name to contain")
+    elif(sys.argv[1] == 'sy' or sys.argv[1] == 'search-year'):
+      try:
+        startY = int(sys.argv[2]) 
+        endY = int(sys.argv[3]) 
+        print('\033[4m'+"Below are books publish between ",startY ,"and " ,endY,'\033[0m')
+        search_year(startY, endY,reader)
+      except:
+        print("Please enter the range of publishing years you would like to search, in formate [start year] [end year] ")
+
+    elif(sys.argv[1] == 'sa' or sys.argv[1] == 'search-author'):
+      try:
+        string = sys.argv[2]
+        print("Below are authors with names containing "+string)
+        print("publications of the author are in purple ")
+        print('')
+        search_author(string,reader)
+      except:
+        print("Please enter the string which you would like the authors' name to contain")
 
 
 
 
-except:
-  print("Please enter an operation, such as st(search books according to title), sy(search according to year), and sa(search according to author)")
+  except:
+    print("Please enter an operation, such as st(search books according to title), sy(search according to year), and sa(search according to author)")
+
+main()
+
 
 
 
